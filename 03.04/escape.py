f8d7b2712c4e171aee0d643e30c139ac109875df
OkhrimenkoA <JackBurns_a32@outlook.com>
2022-04-13 13:52:17 +0300

    added 03.04/escape.py

---
+++ 03.04/escape.py
@@ -0,0 +1,24 @@
+"""
+\n
+\t
+\b
+\'
+\"
+\\
+
+
+"""
+print('Hello\nWorld!')
+print('Hello\tWorld!')
+print('Helloo\b World!')
+print('Hello\nWorld!')
+
+
+print('Escape sequences')
+print('\\a  \tBell (alert)')
+print('\\b  \tBackspace')
+print('\\n  \tNew line')
+print('\\t  \tHorizontal tab')
+print('\\\  \tBackslash \ ')
+print('\\"  \tDouble quotation mark "')
+print("\\'  \tSingle quotation mark '")
---
+++ 10.04/string.py
@@ -0,0 +1,17 @@
+s1 = 'pythonProject16'
+s2 = "python\tProject16\n"
+
+# ASCII
+# KOI-8R
+
+code = 0x26bd
+print(chr(code))
+print('\u26bd')   #\u -размер в байтах 2 байта    \U 4 байта
+print('\U0001f6a3')
+
+print(ord('🚣'))
+print(ord('🚣'))
+
+wave = '~'
+#      0   1   2   3   4
+#      H   E   L   L   O
